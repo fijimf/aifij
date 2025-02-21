2f7c4ccd3002ac63b9a4d57c4721299af94af64a
jamesfrohnhofer <fijimf@gmail.com>
2025-02-21 14:41:01 -0500

    Add standings2025.json to test resources
    This JSON file is added to support future test cases related to 2025 standings data. It ensures compatibility and preparedness for upcoming features or changes.

---
+++ src/test/java/com/fijimf/deepfij/model/scraping/standings/StandingsJsonParseTest.java
@@ -0,0 +1,59 @@
+package com.fijimf.deepfij.model.scraping.standings;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fijimf.deepfij.model.scraping.conference.ConferenceResponse;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StandingsJsonParseTest {
+    @Test
+    public void testParse2014Json() throws IOException {
+        String json = new String(getClass().getClassLoader()
+                .getResourceAsStream("json/standings2014.json")
+                .readAllBytes());
+
+        ObjectMapper mapper = new ObjectMapper();
+        StandingsResponse response = mapper.readValue(json, StandingsResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response.children()).isNotNull();
+        assertThat(response.children()).hasSize(33);
+        response.children().forEach(StandingsJsonParseTest::conferenceAssertions);
+
+    }
+
+
+
+    @Test
+    public void testParse2025Json() throws IOException {
+        String json = new String(getClass().getClassLoader()
+                .getResourceAsStream("json/standings2025.json")
+                .readAllBytes());
+
+        ObjectMapper mapper = new ObjectMapper();
+        StandingsResponse response = mapper.readValue(json, StandingsResponse.class);
+
+        assertThat(response).isNotNull();
+        assertThat(response).isNotNull();
+        assertThat(response.children()).isNotNull();
+        assertThat(response.children()).hasSize(31);
+        response.children().forEach(StandingsJsonParseTest::conferenceAssertions);
+
+
+    }
+
+    private static void conferenceAssertions(ConferenceStanding child) {
+        System.err.println(child.name());
+        assertThat(child).isNotNull();
+        assertThat(child.id()).isNotNull();
+        assertThat(child.uid()).isNotNull();
+        assertThat(child.shortName()).isNotNull();
+        assertThat(child.name()).isNotNull();
+        assertThat(child.abbreviation()).isNotNull();
+        assertThat(child.consolidatedStandings()).isNotNull();
+        assertThat(child.consolidatedStandings()).hasSizeGreaterThanOrEqualTo(1);
+    }
+}
