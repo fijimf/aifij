19ac36ff67a7fa49116edfc1b7d5a2c60e135af4
jamesfrohnhofer <fijimf@gmail.com>
2025-07-19 11:43:57 -0400

    Map tournament game dates to rounds and enhance `TournamentBuilder` with logging for handling excess dates. Update `GameDTO` and `TournamentEntry` to include round details.

--- src/main/java/com/fijimf/deepfij/model/dto/GameDTO.java
+++ src/main/java/com/fijimf/deepfij/model/dto/GameDTO.java
@@ -4,15 +4,18 @@ import com.fijimf.deepfij.model.schedule.Game;
 
 import java.time.LocalDate;
 
-public record GameDTO(long id, int season, LocalDate date, TeamDTO homeTeam, TeamDTO awayTeam, int homeScore,
-                      int awayScore) {
-    public static GameDTO fromGame(Game game) {
+public record GameDTO(long id, int season, LocalDate date, TeamDTO homeTeam, TeamDTO awayTeam, Integer homeTeamSeed, Integer awayTeamSeed, int homeScore,
+                      int awayScore, String round) {
+    public static GameDTO fromGame(Game game, String round) {
         return new GameDTO(game.getId(),
                 game.getSeason().getYear(),
                 game.getDate(),
                 TeamDTO.fromTeam(game.getHomeTeam()),
                 TeamDTO.fromTeam(game.getAwayTeam()),
+                game.getHomeTeamSeed(),
+                game.getAwayTeamSeed(),
                 game.getHomeScore(),
-                game.getAwayScore());
+                game.getAwayScore(),
+        round);
     }
 }
--- src/main/java/com/fijimf/deepfij/service/TournamentBuilder.java
+++ src/main/java/com/fijimf/deepfij/service/TournamentBuilder.java
@@ -4,17 +4,18 @@ import com.fijimf.deepfij.model.dto.GameDTO;
 import com.fijimf.deepfij.model.schedule.Game;
 import com.fijimf.deepfij.model.schedule.Season;
 import com.fijimf.deepfij.repo.GameRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 @Service
 public class TournamentBuilder {
+    private static final Logger log = LoggerFactory.getLogger(TournamentBuilder.class);
     private final GameRepository gameRepository;
 
     @Autowired
@@ -23,18 +24,29 @@ public class TournamentBuilder {
     }
 
     public Tournament build(Season season) {
+        String[] roundsByOrdinalDate = new String[]{"First Four", "First Four", "Round of 64", "Round of 64", "Round of 32", "Round of 32", "Sweet 16", "Elite 8", "Final 4", "National Championship"};
         List<Game> games = gameRepository.findTournamentGamesBySeason(season);
         if (games.isEmpty()) {
             return new Tournament(Collections.emptyList());
         }
-        LocalDate endDate = games.getFirst().getDate();
+        Map<LocalDate, String> dateToRound = new HashMap<>();
+        List<LocalDate> dates = games.stream().map(Game::getDate).distinct().sorted(LocalDate::compareTo).toList();
+        for (int index = 0; index < dates.size(); index++) {
+            LocalDate date = dates.get(index);
+            if (index < roundsByOrdinalDate.length) {
+                dateToRound.put(date, roundsByOrdinalDate[index]);
+            } else {
+                log.warn("Too many dates constructing Tournament");
+            }
+
+        }
 
 
         List<TournamentEntry> nodes = new ArrayList<>();
         List<TournamentEntry> roots = new ArrayList<>();
 
         for (Game g : games) {
-            TournamentEntry newNode = new TournamentEntry(g);
+            TournamentEntry newNode = new TournamentEntry(g, dateToRound.getOrDefault(g.getDate(), null));
             boolean matched = false;
             for (TournamentEntry node : nodes) {
                 if (node.getGame().homeTeam().id().equals(g.getWinner().getId()) && node.getHomeSource() == null) {
@@ -63,9 +75,10 @@ public class TournamentBuilder {
         GameDTO game;
         TournamentEntry homeSource;
         TournamentEntry awaySource;
+        String round;
 
-        public TournamentEntry(Game game) {
-            this.game = GameDTO.fromGame(game);
+        public TournamentEntry(Game game, String round) {
+            this.game = GameDTO.fromGame(game, round);
         }
 
         public GameDTO getGame() {
