8372de57678ee92d9b1b4ae93fdf9b8737e46652
jamesfrohnhofer <fijimf@gmail.com>
2025-11-01 11:11:08 -0400

    Add `PTS_FOR_PTS_AGAINST_COV` statistic to `PointsStatisticModel` and implement covariance calculation in `StatsCalculator`.

--- src/test/java/com/fijimf/deepfij/model/scraping/scoreboard/ScoreboardJsonParseTest.java
+++ src/test/java/com/fijimf/deepfij/model/scraping/scoreboard/ScoreboardJsonParseTest.java
@@ -48,4 +48,32 @@ public class ScoreboardJsonParseTest {
         assertThat(response.sports().getFirst().leagues().getFirst().events()).hasSize(11);
         assertThat(response.events()).hasSize(11);
     }
+
+    @Test
+    public void testParseJson3() throws IOException {
+        String json = new String(Objects.requireNonNull(getClass().getClassLoader()
+                        .getResourceAsStream("json/scoreboard20251103.json"))
+                .readAllBytes());
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new com.fasterxml.jackson.datatype.jsr310.JavaTimeModule());
+
+        ScoreboardResponse response = mapper.readValue(json, ScoreboardResponse.class);
+        assertThat(response).isNotNull();
+        assertThat(response.sports()).isNotNull();
+        assertThat(response.sports()).hasSize(1);
+        assertThat(response.sports().getFirst().leagues()).isNotNull();
+        assertThat(response.sports().getFirst().leagues()).hasSize(1);
+        assertThat(response.sports().getFirst().leagues().getFirst().events()).isNotNull();
+        assertThat(response.sports().getFirst().leagues().getFirst().events()).hasSize(169);
+        assertThat(response.events()).hasSize(169);
+        response.events().forEach(event -> {
+            if (event.uid().equals("s:40~l:41~e:401819834~c:401819834")) {
+                assertThat(event.odds().overUnder()).isEqualTo(153.5);
+                assertThat(event.odds().spread()).isEqualTo(-8.5);
+                assertThat(event.odds().away().moneyLine()).isEqualTo(270);
+                assertThat(event.odds().home().moneyLine()).isEqualTo(-340);
+            }
+        });
+    }
 }
